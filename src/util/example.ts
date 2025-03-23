a7721d8b1917adcb32f080be2dd980ddc188ca8f
Ben Diep <ben_diep@hotmail.com>
2025-03-23 22:28:12 +1100

    WIP - Update dummy test files

--- src/util/example.ts
+++ src/util/example.ts
@@ -10,3 +10,9 @@ function multiply(a: number, b: number): number {
 }
 
 // NOTE: example.ts - Something 3
+
+/**
+ * TODO: example.ts - Something 4
+ * FIXME: example.ts - Something 5
+ * NOTE: example.ts - Something 6
+ */
