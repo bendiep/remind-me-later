8c0296f8512a6515f8a00bf0b7a2fd695ed65683
Ben Diep <ben_diep@hotmail.com>
2025-03-24 19:18:08 +1100

    WIP - 0.1

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ import chalk from "chalk";
 // Define the tag group once.
 const todoTags = "(TODO|FIXME|NOTE)";
 
-// Single-line JS comments
+// Single-line JS comments (e.g., //)
 const jsSingleCommentPattern = `\\/\\/\\s*${todoTags}:?.*`;
 
-// Single-line JSX comments (e.g., {/* NOTE: ... */})
+// Single-line JSX comments (e.g., {/*  */})
 const jsxSingleCommentPattern = `\\{\\/\\*\\s*${todoTags}:?.*?\\*\\/\\}`;
 
-// JS Multiline comments (/* ... */)
+// JS Multiline comments (/* \n */)
 const jsMultiLineCommentPattern = `\\/\\*[\\s\\S]*?\\b${todoTags}\\b:.*?\\*\\/`;
 
-// JSX Multiline comments ({/* ... */})
+// JSX Multiline comments ({/* \n */})
 const jsxMultiLineCommentPattern = `\\{\\/\\*[\\s\\S]*?\\b${todoTags}\\b:.*?\\*\\/\\}`;
 
 const combinedPattern = new RegExp(
@@ -26,7 +26,7 @@ const combinedPattern = new RegExp(
     jsMultiLineCommentPattern,
     jsxMultiLineCommentPattern,
   ].join("|"),
-  "gim"
+  "gi"
 );
 
 async function scanComments(dir = ".") {
